f981c15ae6ed4575a8642004389196cc517d6819
tfavro <wetr9902@gmail.com>
2022-01-01 15:41:17 -0600

    access to api sucessful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
+var apiKey = "733003f902b435aff7ba3aae5ba752e6";
+
 var getWeatherInfo = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=733003f902b435aff7ba3aae5ba752e6");
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=" + apiKey;
+    fetch(apiUrl) 
+    .then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        })     ;
+    });
 };
 getWeatherInfo();
