c93ea7f4cd41ce2f33bd4cebf504dbba9d91555e
tfavro <wetr9902@gmail.com>
2022-01-01 21:48:32 -0600

    pass search results to api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,34 @@
 var apiKey = "733003f902b435aff7ba3aae5ba752e6";
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var forecastContainerEl = document.querySelector("#forecast-container");
+var citySearchTerm = document.querySelector("#repo-search-term");
 
-var getWeatherInfo = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=" + apiKey;
+var getWeatherInfo = function(location) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
     fetch(apiUrl) 
     .then(function(response){
         response.json().then(function(data){
-            console.log(data);
-        })     ;
+            displayWeather(data,location);
+        });
     });
 };
-getWeatherInfo();
+
+var displayWeather = function(weather, location){
+    console.log(weather);
+    console.log(location);
+};
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+
+    if(city){
+        getWeatherInfo(city);
+        cityInputEl.value = "";
+    }
+    else{
+        alert("Please enter a City");
+    }
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
