51a8cdf2e708536b48cd22287ef3945960c8d78d
tfavro <wetr9902@gmail.com>
2022-01-02 21:59:47 -0600

    got lat and lon into variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,33 @@ var apiKey = "733003f902b435aff7ba3aae5ba752e6";
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var forecastContainerEl = document.querySelector("#forecast-container");
-var citySearchTerm = document.querySelector("#repo-search-term");
+var citySearchTerm = document.querySelector("#weather-locaiton");
+var lat = "";
+var lon = "";
 
 var getWeatherInfo = function(location) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
     fetch(apiUrl) 
     .then(function(response){
         response.json().then(function(data){
-            displayWeather(data,location);
+            displayWeather(data,location); 
         });
     });
 };
 
 var displayWeather = function(weather, location){
+    //clear old content
+    forecastContainerEl.textContent="";
+    citySearchTerm.textContent= location;
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    
+    console.log(lon);
+    console.log(lat);
     console.log(weather);
     console.log(location);
 };
+
 
 var formSubmitHandler = function(event){
     event.preventDefault();
