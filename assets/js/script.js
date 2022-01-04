8f882774118e973da5e9e3d0dd03bfd85c0a34a5
tfavro <wetr9902@gmail.com>
2022-01-04 12:34:47 -0600

    current weather section completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var forecastContainerEl = document.querySelector("#forecast-container");
 var citySearchTerm = document.querySelector("#weather-locaiton");
+var ulElement =document.getElementById('list');
+
 var lat = "";
 var lon = "";
 
@@ -15,45 +17,75 @@ var getLocationInfo = function(location) {
         });
     });
 };
-//This will now get the weather information from the onecall api
-var getWeatherInfo = function(lon, lat) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lon + "&lon=" + lat + "&appid=" + apiKey
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-        });
-    });
-   
-    /*fetch(weatherApiUrl) 
-    .then(function(secondResponse){
-        secondResponse.json().then(function(weatherData){
-           //START HERE AND LOOK AT MODULE FOR CREATING THIS AGAIN
-           
-            getLonLatLoc(data,location); 
-        });
-    });*/
-};
-
 
 var getLonLatLoc = function(weather, location){
     //clear old content
     forecastContainerEl.textContent="";
-    citySearchTerm.textContent= location;
+
+    //converting unix to human date
+    var unixTimestamp = weather.dt;
+    var milliseconds = unixTimestamp * 1000;
+    var dateObject = new Date(milliseconds);
+    var humanDateFormat = dateObject.toLocaleDateString("en-US", {month: "numeric"} + "/" +{day: "numeric"} + "/" + {year: "numeric"});
+    
+    //getting icon
+    var iconId = weather.weather[0].icon;
+    var icon = "http://openweathermap.org/img/w/" + iconId + ".png";
+    
+   
+
+    citySearchTerm.textContent= weather.name + " (" + humanDateFormat + ") ";
+    document.getElementById('icon').src = "http://openweathermap.org/img/w/"+ iconId +".png";
+    
+    
     //gets lat and lon coord
 
     var lon = weather.coord.lat;
     var lat = weather.coord.lon;
-    
+    console.log(icon);
     console.log(lon);
     console.log(lat);
     console.log(weather);
     console.log(location);
+   
     getWeatherInfo(lon, lat);
 };
 
 
+//This will now get the weather information from the onecall api
+var getWeatherInfo = function(lon, lat) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lon + "&lon=" + lat + "&units=imperial&appid=" + apiKey
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            displayWeather(data);
+        });
+    });
+};
+var displayWeather = function(getWeather){
+    console.log(getWeather);
+    var temp = document.createElement("li");
+    temp.textContent = "Temp: " + getWeather.current.temp + "℉";
+    ulElement.appendChild(temp);
+
+    var windSpeed = document.createElement("li");
+    windSpeed.textContent = "Wind: " + getWeather.current.wind_speed + " MPH";
+    ulElement.appendChild(windSpeed);
+
+    var humidity = document.createElement("li");
+    humidity.textContent = "Humidity: " + getWeather.current.humidity + " %";
+    ulElement.appendChild(humidity);
+
+    var uvIndex = document.createElement("li");
+    uvIndex.textContent = "UV Index: " + getWeather.current.uvi;
+    ulElement.appendChild(uvIndex);
+    
+};
+
+
+
 var formSubmitHandler = function(event){
     event.preventDefault();
+    ulElement.textContent=""
     var city = cityInputEl.value.trim();
 
     if(city){
